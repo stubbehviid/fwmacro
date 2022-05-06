7c5fc5bea54c9ae524e9afa26b52af5edd9e0fc0
Stubbe Hviid <stubbe@fhviid.eu>
2022-05-06 22:04:13 +0200

    Redesigning library_search

--- fwmacros_search.cmake
+++ fwmacros_search.cmake
@@ -1,3 +1,86 @@
+macro(lib_search_location)
+	set(options STATIC INITIAL_SEARCH DEBUG )
+    set(oneValueArgs ID LIB_NAME ROOT_DIR)
+    set(multiValueArgs )
+    cmake_parse_arguments(LL "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )    
+	
+	# tell what is being done
+    fwmessage(STATUS "------------------------------------------------------")
+    fwmessage(STATUS "lib_search_location (${LL_LIB_NAME})")    
+    fwmessage(STATUS "------------------------------------------------------")
+	fwmessage(STATUS "  INITIAL_SEARCH   = ${LL_INITIAL_SEARCH}")
+    fwmessage(STATUS "  STATIC           = ${LL_STATIC}")
+	fwmessage(STATUS "  DEBUG            = ${LL_DEBUG}")    
+    fwmessage(STATUS "  ID               = ${LL_ID}")
+    fwmessage(STATUS "  LIB_NAME         = ${LL_LIB_NAME}")
+	fwmessage(STATUS "  ROOT_DIR         = ${LL_ROOT_DIR}")	
+
+	set(RESULT_PATH  "${LL_ID}_PATH")
+	if(LL_INITIAL_SEARCH)
+		fwmessage(STATUS "Initial search")		
+		unset( ${RESULT_PATH} )
+	else()
+		fwmessage(STATUS "Normal search")
+	endif()	
+
+	if(NOT EXISTS ${${RESULT_PATH}})
+		if(WIN32)
+			fwmessage(STATUS "Search Windows")
+			if(LL_STATIC)
+			    if(LL_DEBUG)
+				    set(LIB_MASKS ${LL_LIB_NAME}_staticd.lib ${LL_LIB_NAME}d.lib lib${LL_LIB_NAME}_staticd.lib lib${LL_LIB_NAME}d.lib
+					              ${LL_LIB_NAME}_static.lib ${LL_LIB_NAME}.lib lib${LL_LIB_NAME}_static.lib lib${LL_LIB_NAME}.lib)
+				else()
+					set(LIB_MASKS ${LL_LIB_NAME}_static.lib ${LL_LIB_NAME}.lib lib${LL_LIB_NAME}_static.lib lib${LL_LIB_NAME}.lib)
+				endif()
+			else()
+				if(LL_DEBUG)
+					set(LIB_MASKS ${LL_LIB_NAME}d.lib lib${LL_LIB_NAME}d.lib
+					              ${LL_LIB_NAME}.lib lib${LL_LIB_NAME}.lib)
+				else()
+					set(LIB_MASKS ${LL_LIB_NAME}.lib lib${LL_LIB_NAME}.lib)
+				endif()
+			endif()
+		else()
+			fwmessage(STATUS "Search Unix")
+			if(LL_STATIC)
+				if(LL_DEBUG)
+					set(LIB_MASKS ${LL_LIB_NAME}d.a lib${LL_LIB_NAME}d.a)
+				else()
+					set(LIB_MASKS ${LL_LIB_NAME}.a lib${LL_LIB_NAME}.a)
+				endif()
+			else()
+				if(LL_DEBUG)
+					set(LIB_MASKS ${LL_LIB_NAME}d.so lib${LL_LIB_NAME}d.so ${LL_LIB_NAME}d.dylib lib${LL_LIB_NAME}d.dylib)
+				else()
+					set(LIB_MASKS ${LL_LIB_NAME}.so lib${LL_LIB_NAME}.so ${LL_LIB_NAME}.dylib lib${LL_LIB_NAME}.dylib)
+				endif()
+			endif()
+		endif()
+
+		fwmessage(STATUS "LIB_MASKS = ${LIB_MASKS}")
+
+		if(NOT EXISTS ${${RESULT_PATH}})
+			unset(LL_RESULT)
+			foreach( F IN LISTS LIB_MASKS )
+				if(NOT EXISTS ${LL_RESULT})
+					if(EXISTS ${LL_ROOT_DIR}/lib/${F})
+						set(LL_RESULT ${LL_ROOT_DIR}/lib/${F})
+					elseif(EXISTS ${LL_ROOT_DIR}/lib/${LL_LIBNAME}/${F})
+						set(LL_RESULT ${LL_ROOT_DIR}/lib/${LL_LIBNAME}/${F})
+					endif()
+				endif()
+			endforeach()			
+			fwmessage(STATUS "LL_RESULT = ${LL_RESULT}")
+			set(${RESULT_PATH} ${LL_RESULT})
+		endif()
+	else()
+		fwmessage(STATUS "Do nothing - search already successfull")
+	endif()
+endmacro()
+
+
+
 #macro: install_library
 #       Utility macro for handling the installation of libraries
 #
@@ -13,7 +96,7 @@
 
 macro(locate_library)
 	# parse input
-    set(options PREFER_STATIC PREFER_SHARED REQUIRED )
+    set(options PREFER_STATIC PREFER_SHARED REQUIRED HEADER_ONLY )
     set(oneValueArgs ID FOUND_ID LIB_NAME LIB_INCLUDE_FILE )
     set(multiValueArgs )
     cmake_parse_arguments(LL "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )    
@@ -24,134 +107,75 @@ macro(locate_library)
     fwmessage(STATUS "------------------------------------------------------")
     fwmessage(STATUS "  PREFER_STATIC    = ${LL_PREFER_STATIC}")
     fwmessage(STATUS "  PREFER_SHARED    = ${LL_PREFER_SHARED}")
+	fwmessage(STATUS "  HEADER_ONLY      = ${LL_HEADER_ONLY}")
 	fwmessage(STATUS "  REQUIRED         = ${LL_REQUIRED}")
     fwmessage(STATUS "  ID               = ${LL_ID}")
 	fwmessage(STATUS "  FOUND_ID         = ${LL_FOUND_ID}")
     fwmessage(STATUS "  LIB_NAME         = ${LL_LIB_NAME}")
     fwmessage(STATUS "  LIB_INCLUDE_FILE = ${LL_LIB_INCLUDE_FILE}")
     
-	# mark state as success
-	set(${LL_FOUND_ID} ON)
-	
-	# search for library
-	if(NOT "${LL_LIB_NAME}" STREQUAL "")
-	
-		# define the variable used to cache the library paths
-		if(LL_PREFER_SHARED)
-			set(LL_LIB_RELEASE_LABEL 	"${LL_LIB_NAME}_SHARED_LIBRARY_RELEASE")
-			set(LL_LIB_DEBUG_LABEL 		"${LL_LIB_NAME}_SHARED_LIBRARY_DEBUG")
-		else()
-			set(LL_LIB_RELEASE_LABEL 	"${LL_LIB_NAME}_STATIC_LIBRARY_RELEASE")
-			set(LL_LIB_DEBUG_LABEL 		"${LL_LIB_NAME}_STATIC_LIBRARY_DEBUG")
-		endif()
-			
-		# define lists of search therms
-		if(LL_PREFER_SHARED)
-			set(LL_NAMES_RELEASE "${LL_LIB_NAME}.so"          "lib${LL_LIB_NAME}.so"  
-			                     "${LL_LIB_NAME}.lib"         "lib${LL_LIB_NAME}.lib" 
-								 "${LL_LIB_NAME}.a"           "lib${LL_LIB_NAME}.a" 
-								 "${LL_LIB_NAME}_static.lib"  "lib${LL_LIB_NAME}_static.lib")
-			set(LL_NAMES_DEBUG   "${LL_LIB_NAME}d.so"         "lib${LL_LIB_NAME}d.so"  
-			                     "${LL_LIB_NAME}d.lib"        "lib${LL_LIB_NAME}d.lib" 
-								 "${LL_LIB_NAME}d.a"          "lib${LL_LIB_NAME}d.a" 
-								 "${LL_LIB_NAME}_staticd.lib" "lib${LL_LIB_NAME}_staticd.lib")
-			
-		else()
-			set(LL_NAMES_RELEASE "${LL_LIB_NAME}.a"           "lib${LL_LIB_NAME}.a" 
-								 "${LL_LIB_NAME}_static.lib"  "lib${LL_LIB_NAME}_static.lib"
-								 "${LL_LIB_NAME}.so"          "lib${LL_LIB_NAME}.so"  
-			                     "${LL_LIB_NAME}.lib"         "lib${LL_LIB_NAME}.lib" )
-			set(LL_NAMES_DEBUG   "${LL_LIB_NAME}d.a"          "lib${LL_LIB_NAME}d.a" 
-								 "${LL_LIB_NAME}_static.lib"  "lib${LL_LIB_NAME}_static.lib"
-								 "${LL_LIB_NAME}d.so"         "lib${LL_LIB_NAME}d.so"  
-			                     "${LL_LIB_NAME}d.lib"        "lib${LL_LIB_NAME}d.lib" )		
-		endif()
-		fwmessage(STATUS "       - LL_NAMES_RELEASE = ${LL_NAMES_RELEASE}")
-		fwmessage(STATUS "       - LL_NAMES_DEBUG   = ${LL_NAMES_DEBUG}")
-				
-		if(NOT EXISTS ${${LL_LIB_RELEASE_LABEL}})
-			fwmessage(STATUS "Searching for ${LL_LIB_NAME} - release")
-		
-			#define the list of input folders for the search
-			if(WIN32)
-				if(LL_PREFER_STATIC)
-					#set(LL_SEARCH_PATHS "${VCPKG_INSTALLED_PATH}-static" "${VCPKG_INSTALLED_PATH}" ${CMAKE_INSTALL_PREFIX} "c:/Program Files")
-					set(LL_SEARCH_PATHS "${VCPKG_INSTALLED_PATH}-static" ${CMAKE_INSTALL_PREFIX} "c:/Program Files")
-				else()
-					set(LL_SEARCH_PATHS "${VCPKG_INSTALLED_PATH}" "${VCPKG_INSTALLED_PATH}-static" ${CMAKE_INSTALL_PREFIX} "c:/Program Files")
-				endif()
-			else()
-				set(LL_SEARCH_PATHS /usr/local/lib /opt/lib /usr/lib /usr/lib/x86_64-linux-gnu ${CMAKE_INSTALL_PREFIX})
-			endif()
-			fwmessage(STATUS "RELEASE_SEARCH_PATHS = ${LL_SEARCH_PATHS}")
-						
-			# locate release library
-			set(LL_LIB_RELEASE LL_LIB_RELEASE-NOTFOUND)
-			find_library(LL_LIB_RELEASE NAMES ${LL_NAMES_RELEASE} PATHS ${LL_SEARCH_PATHS} PATH_SUFFIXES lib ${LL_LIB_NAME} lib/${LL_LIB_NAME} NO_CACHE)
-			fwmessage(STATUS "Release search result: ${LL_LIB_RELEASE}")
-		else()
-			fwmessage(STATUS "Using cached location for ${LL_LIB_NAME} - release")
-			set(LL_LIB_RELEASE  ${${LL_LIB_RELEASE_LABEL}})
-		endif()
-		
-		if(NOT EXISTS ${${LL_LIB_DEBUG_LABEL}})
-			fwmessage(STATUS "Searching for ${LL_LIB_NAME} - debug")
-		
-			#define the list of input folders for the search
-			if(WIN32)
-				if(LL_PREFER_STATIC)
-					#set(LL_SEARCH_PATHS "${VCPKG_INSTALLED_PATH}-static/debug" "${VCPKG_INSTALLED_PATH}/debug" "${VCPKG_INSTALLED_PATH}" "${VCPKG_INSTALLED_PATH}-static" "${CMAKE_INSTALL_PREFIX}" "c:/Program Files")
-					set(LL_SEARCH_PATHS "${VCPKG_INSTALLED_PATH}-static/debug" "${VCPKG_INSTALLED_PATH}" "${VCPKG_INSTALLED_PATH}-static" "${CMAKE_INSTALL_PREFIX}" "c:/Program Files")
-				else()
-					set(LL_SEARCH_PATHS "${VCPKG_INSTALLED_PATH}/debug" "${VCPKG_INSTALLED_PATH}-static/debug" "${VCPKG_INSTALLED_PATH}" "${VCPKG_INSTALLED_PATH}-static" "${CMAKE_INSTALL_PREFIX}" "c:/Program Files")
-				endif()
-			else()
-				set(LL_SEARCH_PATHS /usr/local/lib /opt/lib /usr/lib /usr/lib/x86_64-linux-gnu ${CMAKE_INSTALL_PREFIX})
-			endif()
-			fwmessage(STATUS "DEBUG_SEARCH_PATHS = ${LL_SEARCH_PATHS}")
-		
-			
-			# locate debug library
-			set(LL_LIB_DEBUG LL_LIB_DEBUG-NOTFOUND)
-			find_library(LL_LIB_DEBUG NAMES ${LL_NAMES_DEBUG} PATHS ${LL_SEARCH_PATHS} PATH_SUFFIXES lib ${LL_LIB_NAME} lib/${LL_LIB_NAME} NO_CACHE)
-			fwmessage(STATUS "Debug search result: ${LL_LIB_RELEASE}")
-		else()
-			fwmessage(STATUS "Using cached location for ${LL_LIB_NAME} - debug")
-			set(LL_LIB_DEBUG  ${${LL_LIB_DEBUG_LABEL}})
-		endif()
-				
-		if(NOT EXISTS ${LL_LIB_DEBUG})
-			set(LL_LIB_DEBUG ${LL_LIB_RELEASE})
-		endif()
-		
-		fwmessage(STATUS "       - LL_LIB_RELEASE = ${LL_LIB_RELEASE}")
-		fwmessage(STATUS "       - LL_LIB_DEBUG   = ${LL_LIB_DEBUG}")
-		
-		set(${LL_LIB_RELEASE_LABEL} ${LL_LIB_RELEASE} CACHE FILEPATH "Location of ${LIB_NAME} library (release)")
-		set(${LL_LIB_DEBUG_LABEL}   ${LL_LIB_DEBUG} CACHE FILEPATH "Location of ${LIB_NAME} library (debug)")
-		
-		fwmessage(STATUS "       - ${LL_LIB_RELEASE_LABEL} = ${${LL_LIB_RELEASE_LABEL}}")
-		fwmessage(STATUS "       - ${LL_LIB_DEBUG_LABEL}   = ${${LL_LIB_DEBUG_LABEL}}")
-		
-		
-		if(EXISTS ${${LL_LIB_RELEASE_LABEL}})
-			foreach(F IN LISTS ${LL_LIB_RELEASE_LABEL})
-				list(APPEND ${LL_ID}_LIBRARIES optimized)
-				list(APPEND ${LL_ID}_LIBRARIES ${F})
-			endforeach()	
-			foreach(F IN LISTS ${LL_LIB_DEBUG_LABEL})
-				list(APPEND ${LL_ID}_LIBRARIES debug)
-				list(APPEND ${LL_ID}_LIBRARIES ${F})
-			endforeach()			
-		else()
-			set(${LL_FOUND_ID} OFF)
-			if(REQUIRED)
-				message(FATAL_ERROR "Did not find required library ${LL_LIB_NAME}")
-			endif()
-		endif()		
+	# define the variable used to cache the library paths
+	if(LL_PREFER_SHARED)
+		set(LL_LIB_RELEASE_LABEL 	"${LL_LIB_NAME}_SHARED_LIBRARY_RELEASE")
+		set(LL_LIB_DEBUG_LABEL 		"${LL_LIB_NAME}_SHARED_LIBRARY_DEBUG")
+	else()
+		set(LL_LIB_RELEASE_LABEL 	"${LL_LIB_NAME}_STATIC_LIBRARY_RELEASE")
+		set(LL_LIB_DEBUG_LABEL 		"${LL_LIB_NAME}_STATIC_LIBRARY_DEBUG")
 	endif()
 	
-	# search for include
+	# ------------------------------------	
+	# search for libraries
+	# ------------------------------------	
+	if(NOT LL_HEADER_ONLY)			
+		if(LL_PREFER_STATIC)
+			# locate release static library
+			lib_search_location(INITIAL_SEARCH        STATIC ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_STATIC_PATH}")
+			lib_search_location(                      STATIC ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+			lib_search_location(                             ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_PATH}")
+			lib_search_location(                             ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+			
+			# locate debug static library
+			lib_search_location(INITIAL_SEARCH DEBUG  STATIC ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_STATIC_PATH}/debug")
+			lib_search_location(               DEBUG  STATIC ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+			lib_search_location(               DEBUG         ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_PATH}/debug")			
+			lib_search_location(               DEBUG         ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+		else()		
+			# locate release shared library
+			lib_search_location(INITIAL_SEARCH               ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_PATH}")
+			lib_search_location(                             ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+			lib_search_location(                      STATIC ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_STATIC_PATH}")
+			lib_search_location(                      STATIC ID LS LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+			
+			# locate debug shared library		
+			lib_search_location(INITIAL_SEARCH DEBUG         ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_PATH}/debug")			
+			lib_search_location(               DEBUG         ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+			lib_search_location(               DEBUG  STATIC ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${VCPKG_INSTALLED_STATIC_PATH}/debug")
+			lib_search_location(               DEBUG  STATIC ID LSD LIB_NAME ${LL_LIB_NAME} ROOT_DIR "${CMAKE_INSTALL_PREFIX}")
+		endif()
+		
+		fwmessage(STATUS "LS_PATH = ${LS_PATH}")
+		fwmessage(STATUS "LSD_PATH = ${LSD_PATH}")
+		
+		
+		set(${LL_LIB_RELEASE_LABEL} "${LS_PATH}" CACHE PATH "${LL_LIB_NAME} include directory" FORCE)
+		set(${LL_LIB_DEBUG_LABEL}   "${LSD_PATH}" CACHE PATH "${LL_LIB_NAME} include directory" FORCE)
+			
+		if(EXISTS ${${LL_LIB_RELEASE_LABEL}})
+			if(EXISTS ${${LL_LIB_DEBUG_LABEL}})
+				list(APPEND ${LL_ID}_LIBRARIES optimized ${${LL_LIB_RELEASE_LABEL}} debug ${${LL_LIB_DEBUG_LABEL}})
+			else()
+				list(APPEND ${LL_ID}_LIBRARIES ${${LL_LIB_RELEASE_LABEL}})
+			endif()
+		else()
+			message(ERROR "Could not locate: ${LL_LIB_RELEASE_LABEL}")
+		endif()
+		
+		fwmessage(STATUS "${LL_ID}_LIBRARIES = ${${LL_ID}_LIBRARIES}")
+	endif()
+	
+	# ------------------------------------	
+	# search for include files
+	# ------------------------------------	
 	if(NOT "${LL_LIB_INCLUDE_FILE}" STREQUAL "")
 		fwmessage(STATUS "Start searching for ${LL_LIB_INCLUDE_FILE}")
 
@@ -163,26 +187,31 @@ macro(locate_library)
 			fwmessage(STATUS "does not exist - so do actual do search for file ${LL_LIB_INCLUDE_FILE}")
 			
 			# Where to look for include file
-			set(SEARCH_PATHS )
-			if(WIN32)
-				list(APPEND SEARCH_PATHS "${VCPKG_INSTALLED_PATH}-static/include" "${VCPKG_INSTALLED_PATH}/include" "c:/Program Files")
+			if(LL_PREFER_STATIC)			
+				set(SEARCH_PATHS "${VCPKG_INSTALLED_PATH}-static/include" "${VCPKG_INSTALLED_PATH}/include")
 			else()
-				list(APPEND SEARCH_PATHS "/usr/local/include" "/usr/include" "/opt/include" "~/include")
+				set(SEARCH_PATHS "${VCPKG_INSTALLED_PATH}/include" "${VCPKG_INSTALLED_PATH}-static/include")
 			endif()
 			list(APPEND SEARCH_PATHS ${CMAKE_INSTALL_PREFIX})
+			if(NOT WIN32)
+				list(APPEND SEARCH_PATHS "/usr/local/include" "/usr/include" "/opt/include" "~/include")
+			endif()
+			
 			fwmessage(STATUS "Search in ${SEARCH_PATHS}")
 				
-			unset(LL_FOUND_INCLUDE_FILE)	
-			find_file (LL_FOUND_INCLUDE_FILE "${LL_LIB_INCLUDE_FILE}" PATHS ${SEARCH_PATHS} PATH_SUFFIXES ${LL_LIB_NAME} lib${LL_LIB_NAME} NO_CACHE)	
-			fwmessage(STATUS "search result: LL_FOUND_INCLUDE_FILE = ${LL_FOUND_INCLUDE_FILE}")
-		
-			if(EXISTS ${LL_FOUND_INCLUDE_FILE})					
-				# extract directory
-				unset(LL_FOUND_INCLUDE_DIR)
-				get_filename_component(LL_FOUND_INCLUDE_DIR ${LL_FOUND_INCLUDE_FILE} DIRECTORY)
-				fwmessage(STATUS "Found ${LL_LIB_INCLUDE_FILE} in folder ${LL_FOUND_INCLUDE_DIR}")
-			endif()	
+			unset(LL_FOUND_INCLUDE_FILE)
+			foreach(F IN LISTS SEARCH_PATHS)
+				if(NOT EXISTS "${LL_FOUND_INCLUDE_DIR}/${LL_LIB_INCLUDE_FILE}")
+					if(EXISTS "${F}/${LL_LIB_INCLUDE_FILE}")
+						set(LL_FOUND_INCLUDE_DIR "${F}")
+					elseif(EXISTS "${F}/${LL_LIB_NAME}/${LL_LIB_INCLUDE_FILE}")
+						set(LL_FOUND_INCLUDE_DIR "${F}/${LL_LIB_NAME}")
+					endif()
+				endif()
+			endforeach()
 			
+			fwmessage(STATUS "search result: LL_FOUND_INCLUDE_DIR = ${LL_FOUND_INCLUDE_DIR}")
+					
 			# create cache entry for the lib include directory			
 			set(${LL_INCLUDE_LABEL} "${LL_FOUND_INCLUDE_DIR}" CACHE PATH "${LL_LIB_NAME} include directory" FORCE)
 			fwmessage(STATUS "Setting ${LL_INCLUDE_LABEL} = ${${LL_INCLUDE_LABEL}}")			
